742f2f6e5a94cd7bb23b1ef4e9f6dd4281adfd3a
MarkusKaas23 <143800614+MarkusKaas23@users.noreply.github.com>
2025-05-31 07:55:53 +0200

    random

--- CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/common/asteroids/IAsteroidSplitter.java
+++ CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/common/asteroids/IAsteroidSplitter.java
@@ -4,7 +4,7 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 
 import dk.sdu.mmmi.cbse.common.data.World;
 
-//IAsteroid
+//IAsteroidSplit
 public interface IAsteroidSplitter {
     void createSplitAsteroid(Entity e, World w);
 
